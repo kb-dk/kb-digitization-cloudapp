95657183f9bda510be3f9e6afe8cf86b4ba1bb23
David Grove Jørgensen <dgj@kb.dk>
2023-07-07 10:38:11 +0200

    added almaService

---
+++ cloudapp/src/app/shared/alma.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  AlertService,
+  CloudAppRestService,
+  HttpMethod,
+  Request
+} from "@exlibris/exl-cloudapp-angular-lib";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlmaService {
+
+  constructor(
+      private restService: CloudAppRestService,
+      private alert: AlertService,
+  ) { }
+
+  scanInItem(itemLink:string, department:string, status:string, workOrderType:string) {
+    let params = { 'op': 'scan', 'department' : department, 'status': status, 'work_order_type=' : workOrderType };
+    let request: Request = {
+      url: itemLink,
+      method: HttpMethod.POST,
+      queryParams:params
+    };
+    return this.restService.call(request);
+  }
+
+  getItemsFromBarcode(barcode:string) {
+    return this.restService.call(`/items?item_barcode=${barcode.trim()}`);
+  }
+
+  handleResponse(reponse:any) {
+    console.log(reponse);
+  }
+}
