a5ec0ae601ea7743911db3e90ee0f9849bc9092f
David Grove Jørgensen <dgj@kb.dk>
2023-07-10 15:53:28 +0200

    merge with master chain call to scanItItem

--- cloudapp/src/app/receive-material/receive-material.component.ts
+++ cloudapp/src/app/receive-material/receive-material.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {tap} from "rxjs/operators";
+import {switchMap, tap} from "rxjs/operators";
 import { DigitizationService } from "../shared/digitization.service";
+import {AlmaService} from "../shared/alma.service";
+import {AlertService} from "@exlibris/exl-cloudapp-angular-lib";
 import {Result} from "../models/Result";
 
 @Component({
@@ -12,10 +14,13 @@ export class ReceiveMaterialComponent implements OnInit {
   @Input() itemFromAlma: any = null;
   @Input() department: string = null;
   @Output() backToMainEvent = new EventEmitter();
+  @Output() loading = new EventEmitter<boolean>();
 
-  constructor(
+    constructor(
       private digitizationService: DigitizationService,
-  ) {}
+      private almaService: AlmaService,
+      private alert: AlertService,
+    ) {}
   ngOnInit(): void {
   }
 
@@ -26,13 +31,24 @@ export class ReceiveMaterialComponent implements OnInit {
     this.digitizationService.receive(`&barcode=${barcode}&step_name=${step_name}`)
         .pipe(
             tap( data=> {console.log(data)}),
-            tap(() => this.backToMain(`${barcode} is received from Digitization successfully.`))
+            switchMap(() => {
+              return this.almaService.scanInItem(this.itemFromAlma.link,this.department,"digitaliseret2","Digiproj","true");
+            })
         )
-        .subscribe();
+        .subscribe({
+            next: result => {
+              this.loading.emit(false);
+              this.backToMain(new Result(true,"Recieved from digitization"));
+            },
+            error: error => {
+              this.loading.emit(false);
+              this.backToMain(new Result(false,error.message));
+            }
+        });
   }
 
   backToMain(message) {
     this.itemFromAlma=null;
-    this.backToMainEvent.emit(new Result(true,message));
+    this.backToMainEvent.emit(message);
   }
 }
