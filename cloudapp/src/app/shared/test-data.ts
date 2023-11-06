1f89d16849e5853ca079fd4dc4c4a49207045f64
zahramousavi <zahramousavidk@gmail.com>
2023-11-06 13:39:19 +0100

    Added HOLDING, USER_REQUEST, INIT_DATA and EMPTY_CONFIG as constants.

---
+++ cloudapp/src/app/shared/test-data.ts
@@ -0,0 +1,75 @@
+export const HOLDING = {
+  "holding_id": "222099563260005763",
+  "created_by": "System",
+  "created_date": "2019-12-16Z",
+  "last_modified_by": "TLR",
+  "last_modified_date": "2023-09-21Z",
+  "originating_system": "ILS",
+  "originating_system_id": "012053104000010-HOL-KGL50",
+  "suppress_from_publishing": "false",
+  "calculated_suppress_from_publishing": "false",
+  "anies": [
+    "<?xml version=\"1.0\" encoding=\"UTF-16\"?><record><leader>00213nx  a22000731i 4500</leader><controlfield tag=\"008\">1910192u    8   4001uueng0000000</controlfield><controlfield tag=\"005\">20230921105412.0</controlfield><datafield ind1=\" \" ind2=\" \" tag=\"583\"><subfield code=\"x\">filnavnssyntax</subfield></datafield><datafield ind1=\"8\" ind2=\" \" tag=\"852\"><subfield code=\"b\">KBL</subfield><subfield code=\"c\">LFKOBKBS</subfield><subfield code=\"h\">KBK 1111,251,3-22a-1979-1983 1984-24/17</subfield></datafield></record>"
+  ]
+}
+
+
+export const USER_REQUEST = {
+  "user_request": [
+  {
+    "title": "Oversigtskort over egnen omkring Hornbæk, Esbønderup og Helsingør / NESA.",
+    "volume": "",
+    "issue": "",
+    "part": "",
+    "barcode": "201003953560",
+    "request_id": "48735562880005763",
+    "additional_id": "6288",
+    "request_type": "WORK_ORDER",
+    "request_sub_type": {
+      "value": "Digiproj",
+      "desc": "Projektdigitalisering"
+    },
+    "mms_id": "99122912149905763",
+    "managed_by_library": "",
+    "managed_by_circulation_desk": "",
+    "managed_by_library_code": "",
+    "managed_by_circulation_desk_code": "",
+    "target_destination": {
+      "value": "Digiproj_1008",
+      "desc": "10008-Pligtafl. kort"
+    },
+    "material_type": {
+      "value": "MAP",
+      "desc": "Map"
+    },
+    "date_of_publication": "",
+    "request_status": "IN_PROCESS",
+    "request_date": "2023-11-02Z",
+    "request_time": "2023-11-02T11:06:09.270Z",
+    "task_name": "In Process",
+    "expiry_date": "2023-11-09Z",
+    "item_id": "231971969210005763"
+  }
+],
+    "total_record_count": 1
+}
+
+export const INIT_DATA = {
+  "user": {
+  "firstName": "my_name",
+      "lastName": "my_last_name",
+      "primaryId": "my_id",
+      "currentlyAtLibCode": "45KBDK_KGL",
+      "currentlyAtDept": "Digiproj_1008",
+      "isAdmin": true
+},
+  "lang": "en",
+    "instCode": "45KBDK_KGL",
+    "urls": {
+  "alma": "http://localhost:4200/",
+      "primo": "https://kbdk-kgl-psb.primo.exlibrisgroup.com"
+},
+  "color": "green"
+}
+
+export const EMPTY_CONFIG = {}
