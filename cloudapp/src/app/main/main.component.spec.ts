e76a89264e64f7418d7e829ec5c52e2eb3639694
zahramousavi <zahramousavidk@gmail.com>
2023-11-06 13:55:15 +0100

    Added app module imports.

--- cloudapp/src/app/main/main.component.spec.ts
+++ cloudapp/src/app/main/main.component.spec.ts
@@ -1,13 +1,25 @@
 import { waitForAsync, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MainComponent } from './main.component';
-import { MaterialModule } from '@exlibris/exl-cloudapp-angular-lib';
+import {AlertModule, MaterialModule} from '@exlibris/exl-cloudapp-angular-lib';
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 describe('MainComponent', () => {
     let component: MainComponent;
     let fixture: ComponentFixture<MainComponent>;
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             imports: [
-                MaterialModule
+                MaterialModule,
+                BrowserModule,
+                BrowserAnimationsModule,
+                AppRoutingModule,
+                HttpClientModule,
+                AlertModule,
+                FormsModule,
+                ReactiveFormsModule
             ],
             declarations: [ MainComponent ],
         })
