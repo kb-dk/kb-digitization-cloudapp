cd8c517159ae49d87ea4e54d4f1c0507479e4f5f
zahramousavi <zahramousavidk@gmail.com>
2023-11-08 14:50:43 +0100

    Created basic send and receive test.

--- cloudapp/src/app/receive-material/receive-material.component.spec.ts
+++ cloudapp/src/app/receive-material/receive-material.component.spec.ts
@@ -1,25 +1,88 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { ReceiveMaterialComponent } from './receive-material.component';
-//
-// describe('ReceiveMaterialComponent', () => {
-//   let component: ReceiveMaterialComponent;
-//   let fixture: ComponentFixture<ReceiveMaterialComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ ReceiveMaterialComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ReceiveMaterialComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { waitForAsync, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ReceiveMaterialComponent } from './receive-material.component';
+import {
+    AlertModule,
+    CloudAppEventsService,
+    CloudAppConfigService,
+    MaterialModule,
+    AlertService
+} from '@exlibris/exl-cloudapp-angular-lib';
+import {BrowserModule, By} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CONFIG, EMPTY_CONFIG, INIT_DATA} from "../shared/test-data";
+import {of} from "rxjs";
+import {MainComponent} from "../main/main.component";
+describe('ReceiveMaterialComponent', () => {
+    let component: ReceiveMaterialComponent;
+    let fixture: ComponentFixture<ReceiveMaterialComponent>;
+    let eventsService: CloudAppEventsService;
+    let configService: CloudAppConfigService;
+    let alertService: AlertService;
+    let spyEvent: jasmine.Spy;
+    let spyConfig: jasmine.Spy;
+    let spyAlert: jasmine.Spy;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                BrowserModule,
+                BrowserAnimationsModule,
+                AppRoutingModule,
+                HttpClientModule,
+                AlertModule,
+                FormsModule,
+                ReactiveFormsModule
+            ],
+            declarations: [ ReceiveMaterialComponent ],
+            providers: [
+                CloudAppEventsService,
+                CloudAppConfigService,
+                AlertService
+            ]
+        })
+            .compileComponents();
+    }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ReceiveMaterialComponent);
+        component = fixture.componentInstance;
+
+        component.deskConfig = CONFIG.desks[4];
+        component.libCode= INIT_DATA.user.currentlyAtLibCode;
+        component.institution= INIT_DATA.instCode;
+        component.almaUrl= INIT_DATA.urls.alma;
+        component.inputLabel= 'Barcode or field583x';
+    });
+
+    describe('should create ', () => {
+        it('receive component', () => {
+            fixture.detectChanges();
+            expect(component).toBeTruthy();
+        });
+    });
+
+    // describe('should input label ', () => {
+    //     it('be "Barcode" if "useMarcField" is "false".', (() => {
+    //         let config = CONFIG;
+    //         config.desks[4].useMarcField = false;
+    //         spyConfig.and.returnValue(of(config));
+    //
+    //         fixture.detectChanges();
+    //
+    //         expect(component.inputLabel).toBe(`Barcode`);
+    //     }));
+    //
+    //     it('be "Barcode or field583x" if "useMarcField" is "true".', (() => {
+    //         let config = CONFIG;
+    //         config.desks[4].useMarcField = true;
+    //         spyConfig.and.returnValue(of(config));
+    //
+    //         fixture.detectChanges();
+    //
+    //         expect(component.inputLabel).toBe(`Barcode or field583x`);
+    //     }));
+    // });
+})
--- cloudapp/src/app/send-material/send-material.component.spec.ts
+++ cloudapp/src/app/send-material/send-material.component.spec.ts
@@ -1,25 +1,67 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-//
-// import { SendMaterialComponent } from './send-material.component';
-//
-// describe('SendMaterialComponent', () => {
-//   let component: SendMaterialComponent;
-//   let fixture: ComponentFixture<SendMaterialComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ SendMaterialComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SendMaterialComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+import { waitForAsync, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { SendMaterialComponent } from './send-material.component';
+import {
+    AlertModule,
+    CloudAppEventsService,
+    CloudAppConfigService,
+    MaterialModule,
+    AlertService
+} from '@exlibris/exl-cloudapp-angular-lib';
+import {BrowserModule, By} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "../app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CONFIG, EMPTY_CONFIG, INIT_DATA} from "../shared/test-data";
+import {of} from "rxjs";
+import {MainComponent} from "../main/main.component";
+describe('SendMaterialComponent', () => {
+    let component: SendMaterialComponent;
+    let fixture: ComponentFixture<SendMaterialComponent>;
+    let eventsService: CloudAppEventsService;
+    let configService: CloudAppConfigService;
+    let alertService: AlertService;
+    let spyEvent: jasmine.Spy;
+    let spyConfig: jasmine.Spy;
+    let spyAlert: jasmine.Spy;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                MaterialModule,
+                BrowserModule,
+                BrowserAnimationsModule,
+                AppRoutingModule,
+                HttpClientModule,
+                AlertModule,
+                FormsModule,
+                ReactiveFormsModule
+            ],
+            declarations: [ SendMaterialComponent ],
+            providers: [
+                CloudAppEventsService,
+                CloudAppConfigService,
+                AlertService
+            ]
+        })
+            .compileComponents();
+    }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SendMaterialComponent);
+        component = fixture.componentInstance;
+
+        component.deskConfig = CONFIG.desks[4];
+        component.libCode= INIT_DATA.user.currentlyAtLibCode;
+        component.institution= INIT_DATA.instCode;
+        component.almaUrl= INIT_DATA.urls.alma;
+        component.inputLabel= 'Barcode or field583x';
+    });
+
+    describe('should create ', () => {
+        it('send component', () => {
+            fixture.detectChanges();
+            expect(component).toBeTruthy();
+        });
+    });
+
+});
